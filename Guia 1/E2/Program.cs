b60e9547db210ecfb1f107259c8e639f54411158
Millar-Emerson <emerson.millar@gmail.com>
2020-04-12 18:02:41 -0300

    ejercicio 2 (no lo copie, lo estoy pasando)

--- Guia 1/E2/Program.cs
+++ Guia 1/E2/Program.cs
@@ -6,7 +6,23 @@ namespace E2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i=0,num=0,num1=0,total=0;
+            Console.WriteLine("ingrese dos numeros enteros");
+            num=int.Parse(Console.ReadLine()); 
+            num1=int.Parse(Console.ReadLine());
+            for(i=0;i<num;i++)
+            {
+                total=total+num;
+            }
+            i=0;
+            Console.WriteLine(num+"."+num1+"="+total);
+            while(num>=num1 && num>0)
+            {
+                i++;
+                num-=num1;
+            }
+            Console.WriteLine("resto="+num);
+            Console.WriteLine("cociente="+i);
         }
     }
 }
