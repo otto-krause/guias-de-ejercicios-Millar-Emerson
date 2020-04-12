051124a8977c78bd75a5dd7dba1142ffd77bfdc4
Millar-Emerson <emerson.millar@gmail.com>
2020-04-12 18:05:41 -0300

    ejercicio 3

--- Guia 1/E3/Program.cs
+++ Guia 1/E3/Program.cs
@@ -6,7 +6,38 @@ namespace E3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num=0;
+            Console.WriteLine("ingrese un numero");
+            num=int.Parse(Console.ReadLine());
+            static void numeropar(int num){
+                if (num%2==0)
+                {
+                    Console.WriteLine("el numero es par");
+                }
+                else{
+                    Console.WriteLine("no es par");
+                }
+            }
+            static void numeroprimo(int num){
+                int i=0,cont=0;
+                for (i=1;i<=num;i++)
+                {
+                    if(num%i==0)
+                    {
+                        cont++;
+                    }
+                }
+                if (cont==2)
+                {
+                    Console.WriteLine("es un numero primo");
+                }
+                else
+                {
+                    Console.WriteLine("no es primo");
+                }
+            }
+            numeropar(num);
+            numeroprimo(num);
         }
     }
 }
