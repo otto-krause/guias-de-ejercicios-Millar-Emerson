10a4ad368d3074c7b19423e4dfac12a1368ad61e
Millar-Emerson <emerson.millar@gmail.com>
2020-04-17 18:01:00 -0300

    veo anime y vuelvo

--- Guia 1/E7/Program.cs
+++ Guia 1/E7/Program.cs
@@ -7,7 +7,8 @@ namespace E7
         static void Main(string[] args)
         {
             Celular A30S=new Celular(false,true,64000,104);
-            Camara Camara_A30S=new Camara(25,794);
+            Camara Camara_A30=new Camara(25,794);
+            Console.WriteLine("Hallo Welt");
         }
     }
 }
