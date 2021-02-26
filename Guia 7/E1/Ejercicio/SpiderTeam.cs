2fce2b189ae12b79c5a62ac591dea84a9a3013e8
Millar-Emerson <emerson.millar@gmail.com>
2021-02-26 04:31:43 -0300

    ejercicio 1

---
+++ Guia 7/E1/Ejercicio/SpiderTeam.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+namespace Ejercicio
+{
+    public class SpiderTeam
+    {
+        List<SpidermanClasico> spidermans;
+
+        public SpiderTeam(List<SpidermanClasico> spidermans)
+        {
+            this.spidermans = spidermans;
+        }
+
+        //public void pelearConKingpin();
+
+        //public string volverACasa();
+    }
+}
